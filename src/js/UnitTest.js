e7af39649b66128d1c2ea920528afac8ce53f5fa
Sérgio Segaty <sergio.segaty@gmail.com>
2020-07-29 13:49:30 -0300

    Feito um novo caso de teste para AddTask()

--- src/js/UnitTest.js
+++ src/js/UnitTest.js
@@ -10,35 +10,38 @@ const testingEnvoirement = () => {
     return main;
 };
 
+//#region Teste de Integração
 /**
  * Testa a integração das outras funções e checa para ver se renderizou
  * a tabela de maneira correta comparando com um mock de OuterHTML.
  */
 const testRenderTable = () => {
-    console.log('Teste de Integração - Render Table');
-    let main = testingEnvoirement();
+        console.log('Teste de Integração - Render Table');
+        let main = testingEnvoirement();
 
-    let tarefa = new Tarefa('Descrição', 'Status', 'Data');
+        let tarefa = new Tarefa('Descrição', 'Status', 'Data');
 
-    main.append(renderTable(tarefa));
+        main.append(renderTable(tarefa));
 
-    let hopeMain = '<div id="main"><table><thead><tr><th>Descrição</th><th>Status</th><th>Data</th><th>Ação</th>' +
-        '</tr></thead><tbody id="tbody"><tr><td>Status</td><td>Descrição</td><td>Data</td><td>' +
-        '<button class="btn btn-danger">Excluir</button></td></tr></tbody><tfoot><td>' +
-        '<input id="inputStatus" type="text" placeholder="Status"></td><td>' +
-        '<input id="inputDesc" type="text" placeholder="Descrição"></td><td>' +
-        '<input id="inputData" type="text" placeholder="dd/mm/AAAA"></td><td>' +
-        '<button class="btn btn-primmary">Adicionar</button></td></tfoot></table></div>';
+        let hopeMain = '<div id="main"><table><thead><tr><th>Descrição</th><th>Status</th><th>Data</th><th>Ação</th>' +
+            '</tr></thead><tbody id="tbody"><tr><td>Status</td><td>Descrição</td><td>Data</td><td>' +
+            '<button class="btn btn-danger">Excluir</button></td></tr></tbody><tfoot><td>' +
+            '<input id="inputStatus" type="text" placeholder="Status"></td><td>' +
+            '<input id="inputDesc" type="text" placeholder="Descrição"></td><td>' +
+            '<input id="inputData" type="text" placeholder="dd/mm/AAAA"></td><td>' +
+            '<button class="btn btn-primmary">Adicionar</button></td></tfoot></table></div>';
 
-    if (main.outerHTML === hopeMain) {
-        console.log('Sucesso! Os elementos da Table são iguais.');
-    } else {
-        console.log('Falha! Os elementos da Table são diferentes');
+        if (main.outerHTML === hopeMain) {
+            console.log('Sucesso! Os elementos da Table são iguais.');
+        } else {
+            console.log('Falha! Os elementos da Table são diferentes');
+        }
+
+        console.log('___________');
     }
-    console.log('___________');
+    //#endregion
 
-}
-
+//#region Teste de Unidade
 /**
  * Testa a função que renderiza o TableHead e compara com um mock de outerHTML.
  */
@@ -53,8 +56,9 @@ const testRenderHead = () => {
     } else {
         console.log("Falha! Os elementos de TableHead são diferentes.");
     }
+
     console.log('-----------');
-}
+};
 
 /**
  * Testa a função que renderiza o TableFooter e compara com um mock de outerHTML.
@@ -74,8 +78,9 @@ const testRenderFooter = () => {
     } else {
         console.log('Falha! Os elementos de TableFooter são diferentes');
     }
+
     console.log('-----------');
-}
+};
 
 /**
  * Testa a função que renderiza o TableRow e compara com um mock de outerHTML.
@@ -89,20 +94,56 @@ const testRenderRow = () => {
 
     let hopeRow = '<tr><td>Teste Stat</td><td>Test Desc</td><td>Teste Date</td>' +
         '<td><button class="btn btn-danger">Excluir</button></td></tr>';
+
     if (row.outerHTML === hopeRow) {
         console.log('Sucesso! Os elementos de TableRow são iguais.')
     } else {
         console.log('Falha! Os elementos de TableRow são diferentes.')
     }
+
     console.log('-----------');
-}
+};
+//#endregion
+
+/**
+ * Testa a função de adicionar uma nova Tarefa. 
+ * Se a validação voltar com erro, ele não adiciona uma nova tarefa e avisa os erros na validação.
+ */
+const testeAddTask = () => {
+    console.log('Teste de Unidade - AddTask');
+
+    let mockTableBody = document.createElement('tbody');
+    let mockTarefa = new Tarefa('Desc Teste', 'Status Teste', 'Data Teste');
+
+    success = addTask(mockTableBody, mockTarefa);
+
+    if (success.valid === true) {
+        console.log('Validação sucedida. Verificando funcionamento do metodo.');
+
+        if (mockTableBody.childNodes.length > 0) {
+            console.log('Tarefa adicionada com sucesso.');
+        } else {
+            console.log('Erro, não foi possível adicionar a Tarefa');
+        }
+    } else {
+        console.log('Validação encontrou erros: \n' + success.message);
+        if (mockTableBody.childNodes.length > 0) {
+            console.log('Erro, mesmo com a validação inválida foi adicionado uma Tarefa.');
+        } else {
+            console.log('Sucesso! A Tarefa não foi adicionado por ter erros de Validação');
+        }
+    }
+
+    console.log('-----------');
+};
 
 const runTests = () => {
     testRenderHead();
     testRenderRow();
     testRenderFooter();
+    testeAddTask();
     testRenderTable();
-}
+};
 
 
 runTests();
